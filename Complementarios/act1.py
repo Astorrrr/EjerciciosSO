328a77edc20bc9824bfb2a4d81a4ff2122788afa
astor <Astorrrr@users.noreply.github.com>
2025-04-15 09:39:18 -0300

    Update act1.py

--- Complementarios/act1.py
+++ Complementarios/act1.py
@@ -1,8 +1,6 @@
-Pi = 3.1415
 pi = 3.1415
 
-R = float(input("Ingrese el radio del circulo"))
-asfsr = float(input("Ingrese el radio del circulo: "))
+r = float(input("Ingrese el radio del circulo: "))
 
 v = 4/3*pi*r**3
 
